7c3f5cee5d8a521fdddaea0bf38d4908e4c2b23b
Daniel Esteban <danyjudoka93@gmail.com>
2026-04-16 21:52:53 -0600

    Eliminated the informative-card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-
-import { InformativeCard } from "./components/informative-card";
 import { Button } from "./components/Button/button";
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button variant={"default"}>Click me</Button>
-      <InformativeCard
-        title="Informative Card"
-        description="This is an informative card component."
-        icon={<span>📘</span>}
-        className="mt-4 w-64"
-      />
     </div>
   );
 }
