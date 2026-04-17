a530c4c0687bdefa5df87d2c5ebe758583e35e63
Leonardo Perez <A01665462@tec.mx>
2026-04-16 21:53:31 -0600

    fix: Remove changes in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Button } from "./components/Button/button";
 import Logo from "./assets/Logo.svg";
-import InputField from "./components/Input/inputField";
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button variant={"default"}>Click me</Button>
-      <InputField />
       <img src={Logo} alt="Logo" />
     </div>
   );
