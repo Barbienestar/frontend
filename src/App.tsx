642b32bf6120ce4aeda35fb5555b77a4a008af0f
EstebanDev1013 <danyjudoka93@gmail.com>
2026-04-19 18:53:29 -0600

    feature/metric card
    * Deleted informative-card component from src/App.tsx
    
    * Added a missing import and deleted unused properties
    
    * created the base of the metric-card component
    
    * Created the component with its props
    
    * Created the stories of the component with its 3 different variants
    
    * Deleted unused importon line 1

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,6 @@ function App() {
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button variant={'default'}>Click me</Button>
       <img src={Logo} alt="Logo" />
-      <InformativeCard
-        title="Informative Card"
-        description="This is an informative card component."
-        icon={<span>📘</span>}
-        className="mt-4 w-64"
-      />
     </div>
   )
 }
