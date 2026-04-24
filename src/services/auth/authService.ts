7919f8781fbec88f1af205cf38c0d97c5dbae899
Leonardo Palatto <A01665462@tec.mx>
2026-04-24 12:55:04 -0600

    Feature/authguards (#10)
    * feat: Basic auth functions
    
    * feat: Consume auth endpoint to store user profile

---
+++ src/services/auth/authService.ts
@@ -0,0 +1,30 @@
+import { signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { auth } from './auth'
+import api from '../api'
+
+interface UserProfile {
+  id: string
+  name: string
+  lastName: string
+  role: string
+  email: string
+}
+
+export const login = async (
+  email: string,
+  password: string
+): Promise<UserProfile> => {
+  const UserCredential = await signInWithEmailAndPassword(auth, email, password)
+  const token = await UserCredential.user.getIdToken()
+  localStorage.setItem('token', token)
+
+  const profileResponse = await api.get<UserProfile>('/auth/me')
+  localStorage.setItem('user', JSON.stringify(profileResponse.data))
+  return profileResponse.data
+}
+
+export const logout = async (): Promise<void> => {
+  await signOut(auth)
+  localStorage.removeItem('token')
+  localStorage.removeItem('user')
+}
