04da26a2a4e0aa07d18dbe31cca5d8ee6d1ca83e
Leonardo Palatto <A01665462@tec.mx>
2026-04-26 13:53:24 -0600

    feat: be-06 complete report creation feature (#18)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Access from './pages/access';
 import ForbiddenPage from './pages/forbidden';
+import ReportarPage from './pages/report';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/access" element={<Access />} />
         <Route path="/forbidden" element={<ForbiddenPage />} />
+        <Route path="/reportar" element={<ReportarPage />} />
       </Routes>
     </BrowserRouter>
   );
