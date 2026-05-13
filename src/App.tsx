7160e0f1a08a2001bc24f9aefb94095188625531
Ian Hernández <67287321+ian-hdzzz@users.noreply.github.com>
2026-05-13 11:32:15 -0600

    feat: mis reportes, graficos (#30)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Access from './pages/access';
 import ForbiddenPage from './pages/forbidden';
+import Report from './pages/report';
+import MyReports from './pages/myReports';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/access" element={<Access />} />
         <Route path="/forbidden" element={<ForbiddenPage />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="/mis-reportes" element={<MyReports />} />
       </Routes>
     </BrowserRouter>
   );
