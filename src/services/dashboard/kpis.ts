f41fa37cbe3e965495f0533daf71e43598a840ba
JorgeECadena <a01660252@tec.mx>
2026-05-20 18:58:05 -0600

    Changed axios petitions to use path params

--- src/services/dashboard/kpis.ts
+++ src/services/dashboard/kpis.ts
@@ -10,24 +10,13 @@ export interface StockReport {
     bottomMedicines: string[];
 }
 
-export const getStockAvgs = async (): Promise<StockAverages> => {
-    // TODO: Get the user's hospital
-    const response = await api.get<StockAverages>('/medicines-hospitals/average-stock', {
-        params: {
-            id_hospital: 62
-        }
-    });
-
+export const getStockAvgs = async (idHospital: number): Promise<StockAverages> => {
+    const response = await api.get<StockAverages>(`/medicines-hospitals/average-stock/${idHospital}`);
     return response.data;
 };
 
-export const getStockReport = async(): Promise<StockReport> => {
-    // TODO: Get the user's hospital
-    const response = await api.get<StockReport>('/medicines-hospitals/stock-report', {
-        params: {
-            id_hospital: 62
-        }
-    });
+export const getStockReport = async(idHospital: number): Promise<StockReport> => {
+    const response = await api.get<StockReport>(`/medicines-hospitals/stock-report/${idHospital}`);
 
     return response.data;
 };
