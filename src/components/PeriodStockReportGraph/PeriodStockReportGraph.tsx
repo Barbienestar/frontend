cd6b23ce43c2595ca10cd2a499f110bb00377839
Leonardo Palatto <A01665462@tec.mx>
2026-05-24 16:59:48 -0600

    feat: open on selected date and make initial period shorter (#62)

--- src/components/PeriodStockReportGraph/PeriodStockReportGraph.tsx
+++ src/components/PeriodStockReportGraph/PeriodStockReportGraph.tsx
@@ -35,7 +35,7 @@ export const PeriodStockReportGraph = ({
   hospitalId,
 }: PeriodStockReportGraphProps) => {
   const [startDate, setStartDate] = useState<Date | undefined>(
-    subMonths(new Date(), 6)
+    subMonths(new Date(), 1)
   );
   const [endDate, setEndDate] = useState<Date | undefined>(new Date());
   const [data, setData] = useState<ReportSnapshot[]>([]);
@@ -127,7 +127,12 @@ export const PeriodStockReportGraph = ({
                   </Button>
                 </PopoverTrigger>
                 <PopoverContent align="end">
-                  <Calendar mode="single" selected={date} onSelect={setDate} />
+                  <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    defaultMonth={date}
+                  />
                 </PopoverContent>
               </Popover>
             );
