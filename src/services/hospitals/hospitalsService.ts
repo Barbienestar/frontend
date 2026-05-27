d772f49aa016cb058ae5e2e7845853273d128542
Daniel Esteban <danyjudoka93@gmail.com>
2026-05-26 23:00:17 -0600

    getCriticalMedicines fetches from the hospitals/{hospitalId}/critical-medicines

--- src/services/hospitals/hospitalsService.ts
+++ src/services/hospitals/hospitalsService.ts
@@ -1,7 +1,15 @@
 import api from '@/services/api';
 import type { HospitalData } from '@/common/HospitalData';
+import type { HospitalCriticalMedicinesResponse } from '@/common/CriticalMedicineData';
 
 export const getMyHospitals = async () => {
   const response = await api.get<HospitalData[]>('/hospitals/my-hospitals');
+  return response.data;
+};
+
+export const getCriticalMedicines = async (hospitalId: number) => {
+  const response = await api.get<HospitalCriticalMedicinesResponse[]>(
+    `/hospitals/${hospitalId}/critical-medicines`
+  );
   return response.data;
 };
